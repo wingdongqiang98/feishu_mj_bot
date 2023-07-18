478a5aebfcf41486a4227a9a056d701208006151
RunningBaby <daweibavip@outlook.com>
2023-07-18 09:37:21 +0800

    add file

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo systemctl status docker
+
+sudo apt-get install -y nginx
+sudo systemctl enable nginx
+sudo apt-get install -y docker-compose
